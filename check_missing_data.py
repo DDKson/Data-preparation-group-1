a5f0bb8ee6b228e806bbcc63e23bb87d83fca66d
DDKson <92723196+DDKson@users.noreply.github.com>
2022-10-28 17:42:38 +0700

    Update check_missing_data.py

--- check_missing_data.py
+++ check_missing_data.py
@@ -1,4 +1,5 @@
+# return dataframe count of null values and percentage of missing values
 def check_missing_data(df):
     total = df.isnull().sum().sort_values(ascending = False)
-    percent = (df.isnull().sum()/df.isnull().count()*100).sort_values(ascending = False)
+    percent = ((df.isnull().sum()/len(df)) *100).sort_values(ascending = False)
     return pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
